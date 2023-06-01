431d3e72bd4bf80a5ec6ce18d05904158247f1e1
Ersin <ersincabuk13@gmail.com>
2023-06-01 08:41:53 +0300

    created: error page

---
+++ error.vue
@@ -0,0 +1,14 @@
+<template>
+  <button @click="handleError">Clear errors</button>
+  <h1>{{ error.statusCode }}</h1>
+  <p>{{ error.statusMessage }}</p>
+  <p>{{ error.message }}</p>
+  {{ error.stack }}
+</template>
+
+<script setup>
+  const props = defineProps({
+    error: Object
+  })
+  const handleError = () => clearError({ redirect: '/' })
+</script>
