2c7f9aeb4f907d42e5f96d2e875ad16da696df17
Ersin <ersincabuk13@gmail.com>
2023-06-03 13:57:51 +0300

    configured: css

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,12 +2,24 @@ import type { Config } from "tailwindcss";
 import { iconsPlugin, getIconCollections } from "@egoist/tailwindcss-icons";
 
 export default<Partial<Config>> {
-  content: [ './pages/**/*.vue', './layouts/**/*.vue' ],
+  content: [ "./pages/**/*.vue", "./layouts/**/*.vue" ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          100: "#E0AAFF",
+          200: "#C77DFF",
+          300: "#9D4EDD",
+          400: "#7B2CBF",
+          500: "#5A189A",
+          600: "#3C096C",
+          700: "#240046",
+          800: "#10002B",
+        }
+      }
+    }
   },
   plugins: [
     iconsPlugin({ collections: getIconCollections(["twemoji"]) })
   ],
 }
-
