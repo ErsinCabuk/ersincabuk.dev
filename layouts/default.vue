27d6b8476329bdbfacb23fe85a19326ef530626a
Ersin <ersincabuk13@gmail.com>
2023-06-09 21:07:42 +0300

    updated

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,12 @@
 <template>
-  <div class="bg-primary-800">
-    <main>
+  <div class="flex flex-col min-h-screen">
+    <main class="flex-grow bg-primary-800 px-24 pt-6">
       <slot/>
     </main>
+    <footer class="flex justify-center items-center h-16 bg-primary-700 text-white">
+      <a href="https://github.com/ErsinCabuk/ersincabuk.dev" class="text-primary-100 hover:underline"> 
+        This website is open source.
+      </a>
+    </footer>
   </div>
 </template>
