392e91dbb2a17e5a9982659daabe4fec3b049a08
Ersin <ersincabuk13@gmail.com>
2023-06-14 10:42:15 +0300

    created

---
+++ pages/blog/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <TitleBox>Blog Categories</TitleBox>
+    <div class="grid grid-cols-2 gap-2">
+      <Box v-for="category in categories"
+            :link="category.link"
+            class="text-center">
+        {{ category.category }}
+      </Box>
+    </div>
+  </div>
+</template>
+
+<script setup>
+  let posts = (await queryContent().find()),
+      categories = Array.from(new Set(posts.map(value => value.category))).map(value => ({ category: value, link: '/blog/'+value.toLowerCase() }))  
+</script>
