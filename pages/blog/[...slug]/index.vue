4266ce995b4e474198b6482624f2ef8924fcd147
Ersin <ersincabuk13@gmail.com>
2023-06-14 09:42:14 +0300

    updated: post not found

--- pages/blog/[...slug]/index.vue
+++ pages/blog/[...slug]/index.vue
@@ -1,51 +1,70 @@
 <template>
   <div>
     <ContentDoc :path="route.path">
+      <!-------------------- POST FOUND ---------------------->
       <template v-slot="{ doc }">
         <div>
-          <TitleBox class="mb-10">Blog: {{ doc.title.charAt(0).toUpperCase() + doc.title.slice(1) }}</TitleBox>
-          <article class="prose prose-lg prose-invert prose-headings:mb-4 prose-h1:font-normal prose-a:no-underline min-w-full ">
+          <TitleBox class="mb-10">Blog: {{ formatString(doc.title) }}</TitleBox>
+          <article class="prose prose-lg prose-invert prose-headings:mb-4 prose-h1:font-normal prose-a:no-underline min-w-full">
             <ContentRenderer :value="doc" />
           </article>
         </div>
       </template>
+      <!-------------------- POST FOUND ---------------------->
   
+      <!-------------------- POST NOT FOUND -------------------->
+      <!-------------------- /blog PAGE -------------------->
       <template #not-found>
-        <div>
-          <div v-if="qc.length == 0">
-            <TitleBox>Post not found</TitleBox>
-          </div>
-          <div v-else>
-            <div v-if="route.path == '/blog'">
-              <TitleBox>Blog Categories</TitleBox>
-              <div class="grid grid-cols-2 gap-2">
-                <Box v-for="category in Array.from(new Set(qc.map(value => value.category))).map(value => ({ category: value, link: '/blog/'+value.toLowerCase() }))"
-                         :link="category.link"
-                         class="text-center">
-                  {{ category.category }}
-                </Box>
-              </div>
-            </div>
-            <div v-else>
-              <TitleBox>Blog: {{ path[path.length - 1].charAt(0).toUpperCase() + path[path.length - 1].slice(1) }}</TitleBox>
-              <div class="grid grid-cols-2 gap-2">
-                <BlogPost v-for="post in qc" 
-                    :key="post._path" 
-                    :title="post.title" 
-                    :description="post.description" 
-                    :date="post.date" 
-                    :link="post._path" />
-              </div>
-            </div>
+        <div v-if="route.path == '/blog'">
+          <TitleBox>Blog Categories</TitleBox>
+          <div class="grid grid-cols-2 gap-2">
+            <Box v-for="category in categories"
+                  :link="category.link"
+                  class="text-center">
+              {{ category.category }}
+            </Box>
           </div>
         </div>
+        <!-------------------- /blog PAGE -------------------->
+
+        <!-------------------- POST NOT FOUND -------------------->
+        <TitleBox v-else-if="posts.length == 0">Not found</TitleBox>
+        <!-------------------- POST NOT FOUND -------------------->
+
+        <!-------------------- POSTS -------------------->
+        <div v-else>
+          <TitleBox>Blog: {{ formatString(path[path.length - 1]) }}</TitleBox>
+          <div class="grid grid-cols-2 gap-2">
+            <BlogPost v-for="post in posts" 
+                :key="post._path" 
+                :title="post.title" 
+                :description="post.description" 
+                :date="post.date" 
+                :link="post._path" />
+          </div>
+        </div>
+        <!-------------------- POSTS -------------------->
       </template>
+      <!-------------------- POST NOT FOUND -------------------->
     </ContentDoc>
   </div>
 </template>
 
 <script setup>
-const route = useRoute()
-const path = route.path.split("/").filter(value => value != "")
-let qc = await queryContent("blog", path[1]).find()
-</script>
+  const route = useRoute()
+  const path = route.path.split("/").filter(value => value != "")
+
+  let posts = (await queryContent().find()),
+      categories = Array.from(new Set(posts.map(value => value.category))).map(value => ({ category: value, link: '/blog/'+value.toLowerCase() }))
+  posts = posts.map(value => {
+            let pathArray = value._id.split(":")
+            pathArray = pathArray.slice(1, pathArray.length - 1)
+            let newValue = { ...value }
+            newValue.pathArray = pathArray
+            return newValue
+          }).filter(value => JSON.stringify(value.pathArray) == JSON.stringify(path))
+
+  function formatString(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1)
+  }
+</script> 
